9c619d336f4659ff1e97f01820e6727acb2eabb9
rmsparajuli <aayusparajuli3@gmail.com>
2018-04-02 16:27:38 +0545

    Initial commit

---
+++ function.js
@@ -0,0 +1,15 @@
+
+
+//    myFunction('a','b');
+// function myFunction(l1,l2) {
+// 	l1.append(l2);
+// 	return l1 ;
+// }
+   //This is for print in javascript
+function get (x) {
+	return x;
+}
+let z = get('c');
+console.log(z);
+console.log("This is Java script");
+onclick = document.getElementById("ramesh").innerHTML = "Hello World!"
