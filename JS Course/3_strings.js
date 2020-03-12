eee7d7d3f50038baa214277d197550a0ed019c99
rmsparajuli <rameshparajuli09@gmail.com>
2020-03-12 14:57:45 +0545

    loop begin

---
+++ JS Course/3_strings.js
@@ -0,0 +1,16 @@
+let str0 = 5 + 5;
+let str1 = 5 + "5";
+let str2 = 5 * "5";
+x = "2";
+const str3 = 5 + parseInt(x);
+
+console.log(str0, str1, str2, str3);
+
+let temp = 20.5;
+let strcat = "Weather of nepal is " + temp + "\u00b0c";
+let strcat2 = `Weather of nepal is ${temp}\u00b0c`;
+console.log(temp);
+console.log(`${strcat}\n${strcat2}`);
+
+let breakExample = `this is break example\nhello `;
+console.log(breakExample);
